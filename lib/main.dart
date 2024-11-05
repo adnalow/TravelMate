8984a0aedff3ce9e11a34bd9a6b140e252a27ea7
KazuMoment <vencygyro@gmail.com>
2024-11-05 13:16:18 +0800

    fixed selected index problem where IndexedStack is just bad at navigating through stateful widgets (part of recent merge)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:travel_mate/main_screen.dart';
 import 'package:travel_mate/firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:travel_mate/discover_utils/utils.dart';
 import 'package:travel_mate/wrapper.dart';
 
 
--- lib/wrapper.dart
+++ lib/wrapper.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:travel_mate/Screens/login.dart';
 import 'package:travel_mate/main_screen.dart';
-import 'package:travel_mate/Screens/login.dart';
 
 class Wrapper extends StatefulWidget {
   const Wrapper({super.key});
